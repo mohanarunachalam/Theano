12748aa6b7ae222140f303aa67328e41f2a20273
Gokula Krishnan <gokul.uf@gmail.com>
2016-04-02 14:03:06 +0530

    added more verbose outputs

--- theano/tensor/opt.py
+++ theano/tensor/opt.py
@@ -2796,9 +2796,12 @@ def local_subtensor_merge(node):
                     i+=1
                     # temp_node = temp_node.inputs[0].owner
                     lsm_output = temp_node.outputs[0]
+                    f.write("Node's output: {} \n".format(str(temp_node.outputs)))
+                    f.write("NOP's clients: {} \n".format(str(lsm_output.clients)))
                     new_lsm = lsm_output.clients[0]
                     #new_lsm[0] is the next lsm in the chain
                     temp_node = new_lsm[0]
+                    
                 if i > 2:
                     f.write("Gotcha!\n")
                 f.write("Chain Length: {}\n".format(str(i)))
